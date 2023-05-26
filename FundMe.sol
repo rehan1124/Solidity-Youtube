fda5a0474279251f0c5ea6e76eb1a6c73b00c7a3
srehan1 <syed.rehan@shoppertrak.com>
2023-05-26 18:01:18 +0530

    Chainlink data feed demo

---
+++ FundMe.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+    uint256 public minimumUsd = 50 * 1e18;
+
+    function getPrice() public view returns (uint256) {
+        // Contract address: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+
+        // Price of ETH in terms of USD
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(
+        uint256 ethAmount
+    ) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethInUsd = (ethPrice * ethAmount) * 1e18;
+        return ethInUsd;
+    }
+
+    function fund() public payable {
+        require(
+            getConversionRate(msg.value) >= minimumUsd,
+            "Minimum 50 USD is required."
+        );
+    }
+}
