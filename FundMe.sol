d4f43c726d8a12dc962910b2afeb4a3aaa1f172d
srehan1 <syed.rehan@shoppertrak.com>
2023-05-27 17:30:15 +0530

    Fixes

--- FundMe.sol
+++ FundMe.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
-import "contracts/PriceConverter.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
     // --- State variables ---
