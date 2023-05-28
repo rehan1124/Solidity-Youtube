61d9344a7890bd1516a530db318b0e161442fb2d
srehan1 <syed.rehan@shoppertrak.com>
2023-05-28 14:38:31 +0530

    receive() example

---
+++ FallBackExample.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+contract FallBackExample {
+    uint256 public result;
+
+    receive() external payable {
+        result = 1;
+    }
+}
